08e6d059092e8a55e9cf8d777153673b392b37d3
Voloshin-Sergei <4voloshin@gmail.com>
2020-12-16 00:57:46 +0300

    feat: add layout

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
+import React from 'react';
 import "./App.css";
-import Test from "./components/Test/Test.jsx"
 
 function App() {
-  const hello = "Hello World!";
-  const array = [{ a: 1 }, { a: 2 }];
-  let bool = true;
+
   return (
     <div className="App">
-      {hello}
-      {array.map((item) => {
-        return item.a;
-        // console.log(item)
-      })}
-
-      {bool ? hello : ""}
-
-      <Test name = {"Nikita"} />
-    </div>
+    <header className="header container">
+      <h1 className="header-title">Covid 19 statistic</h1>
+    </header>
+    <main className="main container">
+      <div className="infected">
+        <div className="global">global</div>
+        <div className="country">country</div>
+      </div>
+      <div className="map">map</div>
+      <div className="day-statistic">
+        <div className="day-statistic-data">
+          <div className="deaths">deaths</div>
+          <div className="lives">lives</div>
+        </div>
+        <div className="graph">graph</div>
+      </div>
+    </main>
+    <footer className="footer container">RSS</footer>
+  </div>
   );
 }
 
