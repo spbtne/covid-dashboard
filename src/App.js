c7e9ef73efd908dcf58ac85fd994db4abf3c130b
Nikita Tushin <spbtne@gmail.com>
2020-12-17 21:21:08 +0300

    add: create header layout

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
-import React from 'react';
+import React from "react";
 import "./App.css";
+import Header from "./header/header";
 
 function App() {
-
   return (
     <div className="App">
-    <header className="header container">
-      <h1 className="header-title">Covid 19 statistic</h1>
-    </header>
-    <main className="main container">
-      <div className="infected">
-        <div className="global">global</div>
-        <div className="country">country</div>
-      </div>
-      <div className="map">map</div>
-      <div className="day-statistic">
-        <div className="day-statistic-data">
-          <div className="deaths">deaths</div>
-          <div className="lives">lives</div>
+      <header className="header container">
+        <Header/>
+      </header>
+      <main className="main container">
+        <div className="infected">
+          <div className="global">global</div>
+          <div className="country">country</div>
         </div>
-        <div className="graph">graph</div>
-      </div>
-    </main>
-    <footer className="footer container">RSS</footer>
-  </div>
+        <div className="map">map</div>
+        <div className="day-statistic">
+          <div className="day-statistic-data">
+            <div className="deaths">deaths</div>
+            <div className="lives">lives</div>
+          </div>
+          <div className="graph">graph</div>
+        </div>
+      </main>
+      <footer className="footer container">RSS</footer>
+    </div>
   );
 }
 
